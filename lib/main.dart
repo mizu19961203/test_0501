9e9aca3f47b51d38b17d4802e2399b6d805ab0bb
naoki <Running Gradle task 'assembleDebug>
2021-05-02 12:46:11 +0900

    hara`s first commit

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '私は原なおきです',
+              '私は原なおきです。本日もよろしくお願いいたします'
             ),
             Text(
               '$_counter',
