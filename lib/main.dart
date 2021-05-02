f614cf792f137dab8fa320148b01555d7f056446
naoki <Running Gradle task 'assembleDebug>
2021-05-02 12:58:46 +0900

    テキスト変更しました

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '私は原なおきです。本日もよろしくお願いいたします'
+              '私は原なおきです。3日間お疲れ様でした'
             ),
             Text(
               '$_counter',
